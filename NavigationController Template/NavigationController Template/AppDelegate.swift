7827574d6b5ecc1e1a9d14895b7c4cdc7127bb10
Ahmed Khalaf <pxlshpr@gmail.com>
2017-04-19 15:20:43 +0500

    add navigation controller template

---
+++ NavigationController Template/NavigationController Template/AppDelegate.swift
@@ -0,0 +1,69 @@
+//
+//  AppDelegate.swift
+//  NavigationController Template
+//
+//  Created by Ahmed Khalaf on 19/4/17.
+//  Copyright © 2017 pxlshpr. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  
+  var window: UIWindow?
+  var app: Application?
+  
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    let window = UIWindow(frame: UIScreen.main.bounds)
+    self.app = Application(window: window)
+    
+    self.window = window
+    self.app?.navigation.start()
+    return true
+  }
+}
+
+class Application {
+  private let window: UIWindow
+  lazy var navigation: Navigation = Navigation(
+    window: self.window,
+    navigationController: NavigationController(),
+    application: self
+  )
+  
+  init(window: UIWindow) {
+    self.window = window
+  }
+}
+
+class NavigationController: UINavigationController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    //self.setNavigationBarHidden(true, animated: false)
+  }
+}
+
+class Navigation {
+  private let navigationController: UINavigationController
+  private let application: Application
+  
+  init(window: UIWindow, navigationController: UINavigationController, application: Application) {
+    self.application = application
+    self.navigationController = navigationController
+    window.rootViewController = self.navigationController
+    window.makeKeyAndVisible()
+  }
+  
+  func start() {
+    self.showFirstScreen()
+  }
+  
+  func showFirstScreen() {
+    let viewController = UIViewController()
+    viewController.view.backgroundColor = .white
+    self.navigationController.pushViewController(viewController, animated: false)
+  }
+}
+
+
---
+++ NavigationController Template/NavigationController Template/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NavigationController Template
+//
+//  Created by Ahmed Khalaf on 19/4/17.
+//  Copyright © 2017 pxlshpr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
